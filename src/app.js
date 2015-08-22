151d9a575a1b4524896b02d20148951e015bcddc
Roberto <topikito@gmail.com>
2015-08-23 01:35:22 +0200

    N - Initial commit

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import 'bootstrap';
+import 'bootstrap/css/bootstrap.css!';
+
+export class App {
+  configureRouter(config, router){
+    config.title = 'Aurelia';
+    config.map([
+      { route: ['','welcome'],  name: 'welcome',      moduleId: 'welcome',      nav: true, title:'Welcome' },
+      { route: 'users',         name: 'users',        moduleId: 'users',        nav: true, title:'Github Users' }
+    ]);
+
+    this.router = router;
+  }
+}
